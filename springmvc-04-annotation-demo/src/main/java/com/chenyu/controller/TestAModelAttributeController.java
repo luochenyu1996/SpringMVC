b508c5583dce3781ccf5773a84d5c0ada79e7ed7
luochenyu1996 <2856137514@qq.com>
2021-07-11 11:32:48 +0800

    增加了TestAModelAttributeController

---
+++ springmvc-04-annotation-demo/src/main/java/com/chenyu/controller/TestAModelAttributeController.java
@@ -0,0 +1,10 @@
+package com.chenyu.controller;
+
+/**
+ * @program: SpringMVC
+ * @description: 测试@AModelAttribute
+ * @author: chenyu
+ * @create: 2021-07-11 11:30
+ */
+public class TestAModelAttributeController {
+}
